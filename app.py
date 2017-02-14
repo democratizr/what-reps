7400bb19be841c40477a132bba35957c247476b7
Ben Reilly <benwreilly@gmail.com>
2017-02-13 19:08:31 -0800

    break up a bit of this and fix the address parsing

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import requests
+from urllib import unquote_plus
+
 from chalice import Chalice, BadRequestError
 
 
@@ -19,20 +21,31 @@ def get_coordinates(address):
     return location['lat'], location['lng']
 
 
+def feds(lat, lng):
+    url = sunlight_t.format(lat=lat, long=lng)
+    federal_reps = requests.get(url).json().get('results')
+    return federal_reps
+
+
+def state_reps(lat, lng):
+    url = openstates_t.format(lat=lat, long=lng)
+    state_reps = requests.get(url).json()
+    return state_reps
+
+
 def search(address):
     """
     Return all the legislators you got, based on address.
     """
+    address = unquote_plus(address)
     lat, lng = get_coordinates(address)
 
     # These could be done in parallel...somehow?
-    url = openstates_t.format(lat=lat, long=lng)
-    state_reps = requests.get(url).json()
-    url = sunlight_t.format(lat=lat, long=lng)
-    federal_reps = requests.get(url).json().get('results')
+    s_reps = state_reps(lat, lng)
+    federal_reps = feds(lat, lng)
     return {
         'location': (lat, lng),
-        'state': state_reps,
+        'state': s_reps,
         'federal': federal_reps
     }
 
@@ -42,6 +55,8 @@ def index():
     return {
         'endpoints': {
             '/': 'This page.',
+            '/state-reps/?lat={lat}&?lng={lng}': 'Just the state representatives',
+            '/fed-reps/?lat={lat}&?lng={lng}': 'Just the federal representatives',
             '/search/{address}': "Find all Federal and State representatives for this address."
         }
     }
@@ -53,3 +68,19 @@ def searchview(address):
         return search(address)
     except:
         raise BadRequestError('Cannot find address: ' + address)
+
+
+@app.route('/fed-reps/')
+def fedview():
+    params = app.current_request.query_params
+    lat = params.get('lat')
+    lng = params.get('lng')
+    return feds(lat, lng)
+
+
+@app.route('/state-reps/')
+def fedview():
+    params = app.current_request.query_params
+    lat = params.get('lat')
+    lng = params.get('lng')
+    return state_reps(lat, lng)
